cda9e14477f458a17b71ccb9d85b5747b78daa16
Armstrong Shi <ashi@advent.com>
2019-03-29 15:02:50 +0800

    Public API

---
+++ APXWS/APXRESTfulAPI/HttpClientProxy.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Advent.ApxRestApiExample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+    using System.Text;
+    using Newtonsoft.Json;
+
+    public class ApxToken
+    {
+        public string access_token
+        {
+            get;
+            set;
+        }
+        public int expires_in
+        {
+            get;
+            set;
+        }
+        public string token_type
+        {
+            get;
+            set;
+        }
+    }
+
+    public class HttpClientProxy : IDisposable
+    {
+        private HttpClient client;
+
+        public HttpClientProxy(string webServer, string username, string password)
+        {
+            ApxToken token = this.GetToken(webServer, username, password);
+            this.client = this.CreateHttpClientInstance(webServer, token.access_token);
+        }
+
+        private ApxToken GetToken(string webServer, string username, string password)
+        {
+            // this is to bypass certificate validation error. 
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+           
+            string requestUri = string.Format("https://{0}:5001/connect/token", webServer);
+            HttpContent content = new FormUrlEncodedContent(new Dictionary<string, string>()
+            {
+                { "client_id", "ro.APXPublicAPIClient" },
+                { "client_secret", "advs" },
+                { "grant_type", "password" },
+                { "scope", "APXPublicAPI" },
+                { "username", username },
+                { "password", password }
+            });
+            
+            HttpResponseMessage response = new HttpClient().PostAsync(requestUri, content).Result.EnsureSuccessStatusCode();
+            string json = response.Content.ReadAsStringAsync().Result;
+            ApxToken token = JsonConvert.DeserializeObject<ApxToken>(json);
+            return token;
+        }
+
+        private HttpClient CreateHttpClientInstance(string webserver, string accessToken)
+        {
+            HttpClient client = new HttpClient();
+            //client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", accessToken));
+            client.BaseAddress = new Uri(string.Format("http://{0}", webserver));
+
+            return client;
+        }
+
+        public string HttpGet(string relativeUri)
+        {
+            string requestUrl = string.Format("{0}/{1}", this.client.BaseAddress.AbsoluteUri, relativeUri);
+            HttpResponseMessage response = this.client.GetAsync(requestUrl).Result;
+            response.EnsureSuccessStatusCode();
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        public string HttpPatch(string relativeUri, string jsonString)
+        {
+            string requestUrl = string.Format("{0}/{1}", this.client.BaseAddress.AbsoluteUri, relativeUri);
+            HttpRequestMessage request = new HttpRequestMessage(new HttpMethod("Patch"), requestUrl);
+            request.Content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = this.client.SendAsync(request).Result;
+            response.EnsureSuccessStatusCode();
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ APXWS/APXRESTfulAPI/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Advent.ApxRestApiExample
+{
+    using System;
+    using System.Configuration;
+    using System.IO;
+    using System.Runtime.Serialization;
+    using System.Runtime.Serialization.Json;
+    using System.Text;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClientProxy proxy = new HttpClientProxy("vmapxba9.advent.com", "api", "advs");
+            string result = proxy.HttpGet("apxlogin/api/odata/v1/portfolios");
+        }
+    }
+}
