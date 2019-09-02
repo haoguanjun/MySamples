9d5d48ad267492d8d620fb9e8eb1a7a45d412028
Armstrong Shi <ashi@advent.com>
2019-09-01 22:26:39 -0700

    AdvIdentityClientSamples - IdentityModel

---
+++ AdvIdentityClientSamples/AdvIdentityModelSample/AdvIdentityModelSample/AdvIdentityClient.cs
@@ -0,0 +1,113 @@
+﻿
+namespace AdvIdentityModelSample
+{
+    using IdentityModel.Client;
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    class AdvIdentityClient
+    {
+        public string Issuer
+        {
+            get
+            {
+                if (this.discovery == null)
+                {
+                    return null;
+                }
+
+                return this.discovery.Issuer;
+            }
+            set
+            {
+                this.discovery = this.GetDiscoveryDocument(value);
+            }
+        }
+
+        public string ClientId
+        { get; set; }
+        public string ClientSecret
+        { get; set; }
+        public string Scope
+        { get; set; }
+
+        private DiscoveryDocumentResponse discovery;
+
+        private DiscoveryDocumentResponse GetDiscoveryDocument(string Issuer)
+        {
+            if (string.IsNullOrEmpty(Issuer))
+            {
+                throw new ArgumentNullException("Issuer can't be null or empty.");
+            }
+
+            // bypass self-signed Certificate error
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+
+            var client = new HttpClient();
+            var response = client.GetDiscoveryDocumentAsync(Issuer);
+            response.Wait();
+            if (response.IsFaulted)
+            {
+                throw response.Exception;
+            }
+
+            return response.Result;
+        }
+
+        public TokenResponse Login(string username, string password)
+        {
+            // bypass self-signed Certificate error
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+
+            var client = new HttpClient();
+            var response = client.RequestPasswordTokenAsync(new PasswordTokenRequest
+            {
+                Address = this.discovery.TokenEndpoint,
+                ClientId = this.ClientId,
+                ClientSecret = this.ClientSecret,
+                Scope = this.Scope,
+                UserName = username,
+                Password = password
+            });
+
+            response.Wait();
+            if (response.IsFaulted)
+            {
+                throw response.Exception;
+            }
+
+            return response.Result;
+        }
+
+        public TokenResponse Login()
+        {
+            // bypass self-signed Certificate error
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+
+            var handler = new HttpClientHandler();
+            handler.UseDefaultCredentials = true;
+
+            var client = new HttpClient(handler);
+            var response = client.RequestTokenAsync(new TokenRequest
+            {
+                Address = this.discovery.TokenEndpoint,
+                ClientId = this.ClientId,
+                ClientSecret = this.ClientSecret,
+                Parameters = {
+                    { "scope", this.Scope}
+                },
+                GrantType = "WindowsAuth",
+            });
+
+            response.Wait();
+            if (response.IsFaulted)
+            {
+                throw response.Exception;
+            }
+
+            return response.Result;
+        }
+    }
+}
---
+++ AdvIdentityClientSamples/AdvIdentityModelSample/AdvIdentityModelSample/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace AdvIdentityModelSample
+{
+    using System;
+    using IdentityModel.Client;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new AdvIdentityClient
+            {
+                Issuer = "https://vmapxba9.advent.com:5001",
+                ClientId = "ro.APXAPIClient",
+                ClientSecret = "advs",
+                Scope = "apxapi offline_access"
+            };
+
+            Console.WriteLine("Windows-NT User Login");
+            var response1 = client.Login();
+            Program.PrintTokens(response1);
+
+            Console.WriteLine("APX User Login");
+            var response2 = client.Login("api", "advs");
+            Program.PrintTokens(response2);
+        }
+
+        static void PrintTokens(TokenResponse reponse)
+        {
+            Console.WriteLine("========================================================");
+            Console.WriteLine("IdentityToken");
+            Console.WriteLine(reponse.IdentityToken);
+            Console.WriteLine("========================================================");
+            Console.WriteLine("AccessToken");
+            Console.WriteLine(reponse.AccessToken);
+            Console.WriteLine("========================================================");
+            Console.WriteLine("RefreshToken");
+            Console.WriteLine(reponse.RefreshToken);
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Press Enter to continue.");
+            Console.ReadLine();
+        }
+    }
+}
